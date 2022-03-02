d9437a22d3c6bd410ca30c8a1ac6feada28f5620
Kishan <kishan.shashikumar@gmail.com>
2022-03-02 16:50:28 +0100

    Initial commit

---
+++ Two Sum/main.py
@@ -0,0 +1,22 @@
+#from typing import List
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        traversed = {}
+        
+        for i, value in enumerate(nums):
+            req_num = target - nums[i]
+            
+            if req_num in traversed:
+                return [i, traversed[req_num]]
+            
+            traversed[value] = i
+        
+        
+        
+        
+nums = [2,7,11,15]
+target = 9
+sum = Solution()
+indices = sum.twoSum(nums, target)
+print(indices)
